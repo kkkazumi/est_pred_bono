eb2fd0fef8315d1afeae2091120215bf940ba00c
kazumi <kazumi.kumagai@riken.jp>
2022-11-15 23:05:13 -0800

    indented

--- before_estimate.sh
+++ before_estimate.sh
@@ -1,16 +1,17 @@
 #!/bin/bash -eu
 
-#echo N_num
-#num=$[$N_num-1]
-#sed -i -e "s/define N $num/define N $N_num/" estimate.cpp
-for username in B9001_CN B9001_ML B9002_CN B9002_ML B9003_CN B9003_ML
+for username in B9001 B9002 B9003
 do
-    filename="./bono_datamaker/data/sudata/"$username"_factor.csv"
-    LEN=`wc -l $filename`
-	ARR=(${LEN//,/ })
-	echo $ARR
-	sed -i -e "s/define N 30/define N $ARR/" estimate.cpp
-	make estimate
-	./estimate
-	sed -i -e "s/define N $ARR/define N 30/" estimate.cpp
+	for condition in CN ML
+	do
+		#B9001_ML B9002_CN B9002_ML B9003_CN B9003_ML
+		filename="./bono_datamaker/data/sudata/"$username"_"$condition"_factor.csv"
+		LEN=`wc -l $filename`
+		ARR=(${LEN//,/ })
+		echo $ARR
+		sed -i -e "s/define N 30/define N $ARR/" estimate.cpp
+		make estimate
+		./estimate ${username} ${condition}
+		sed -i -e "s/define N $ARR/define N 30/" estimate.cpp
+	done
 done
